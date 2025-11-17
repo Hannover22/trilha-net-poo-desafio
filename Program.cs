3960224dd477c4326839281503be1791170ee954
Hannover22 <gabrielcaramori4@gmail.com>
2025-11-17 16:42:27 -0300

    Projeto pronto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone("15276152", "17", "67125", 64);
+iphone.Ligar(); 
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
+
+Smartphone tijolao = new Nokia("172653232", "tijolao", "16725712", 32);
+tijolao.Ligar();
+tijolao.ReceberLigacao();
+tijolao.InstalarAplicativo("Jogo da cobrinha");
